795571a312366693a44fe7fb81574c836e51a9a1
Guillermo Gonzalez <josegmgr@gmail.com>
2025-06-30 02:50:15 -0500

    fix(conexion): Ahora los env son tomados correctamente tanto en produccion y local [Informacion de acceso protegida].

--- src/main/java/com/eloskar/restaurante/util/Conexion.java
+++ src/main/java/com/eloskar/restaurante/util/Conexion.java
@@ -1,20 +1,29 @@
 package com.eloskar.restaurante.util;
+
 import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()    // No lanza error si no hay .env
+            .ignoreIfMalformed()  // No lanza error si hay líneas raras
+            .load();
 
     public static Connection getConnection() {
         try {
-            String url = dotenv.get("DB_URL");
+            String url = System.getenv("DB_URL");
             String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-            String usr = dotenv.get("DB_USER");
-            String pwd = dotenv.get("DB_PASSWORD");
-            System.out.println(url + usr + pwd);
+            String usr = System.getenv("DB_USER");
+            String pwd = System.getenv("DB_PASSWORD");
+
+            if (url == null) url = dotenv.get("DB_URL");
+            if (usr == null) usr = dotenv.get("DB_USER");
+            if (pwd == null) pwd = dotenv.get("DB_PASSWORD");
+
             Class.forName(driver);
             return DriverManager.getConnection(url, usr, pwd);
         } catch (ClassNotFoundException | SQLException ex) {
