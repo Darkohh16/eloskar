8637a9a1a4c84ae3370cf2cd788303d96b0e1e7c
Guillermo Gonzalez <josegmgr@gmail.com>
2025-06-30 01:35:42 -0500

    fix(conexion): Ahora los env son tomados correctamente en local.

--- src/main/java/com/eloskar/restaurante/util/Conexion.java
+++ src/main/java/com/eloskar/restaurante/util/Conexion.java
@@ -1,19 +1,20 @@
 package com.eloskar.restaurante.util;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
 
+    private static final Dotenv dotenv = Dotenv.load();
+
     public static Connection getConnection() {
         try {
-            String url = "jdbc:sqlserver://sqlserverdarkohh.cti60io0mpe0.us-east-2.rds.amazonaws.com:1433;" +
-                    "databaseName=eloskar;encrypt=false;";
+            String url = dotenv.get("DB_URL");
             String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-            String usr = "Admin";
-            String pwd = "111100Jgmgr!";
-
+            String usr = dotenv.get("DB_USER");
+            String pwd = dotenv.get("DB_PASSWORD");
+            System.out.println(url + usr + pwd);
             Class.forName(driver);
             return DriverManager.getConnection(url, usr, pwd);
         } catch (ClassNotFoundException | SQLException ex) {
